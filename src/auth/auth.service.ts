2058d95f74f0dbf6e4e5303813e2fb950dc0a0dd
glrmrissi <guilhermerissi04112006@gmail.com>
2025-09-16 19:20:43 -0300

    feat: implement AuthStateService to manage authentication state and token storage

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, inject } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
+import { PLATFORM_ID } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class AuthStateService {
+  private platformId = inject(PLATFORM_ID);
+  private isLoggedInSubject = new BehaviorSubject<boolean>(this.hasToken());
+  isLoggedIn$: Observable<boolean> = this.isLoggedInSubject.asObservable();
+
+  login(token: string) {
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('token', token);
+    }
+    this.isLoggedInSubject.next(true);
+  }
+
+  logout() {
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.removeItem('token');
+    }
+    this.isLoggedInSubject.next(false);
+  }
+
+  getToken(): string | null {
+    if (isPlatformBrowser(this.platformId)) {
+      return localStorage.getItem('token');
+    }
+    return null;
+  }
+
+  private hasToken(): boolean {
+    if (isPlatformBrowser(this.platformId)) {
+      return !!localStorage.getItem('token');
+    }
+    return false;
+  }
+}
