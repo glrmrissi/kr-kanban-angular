c43d3822b16285d9482afaff8b0292145f19308f
glrmrissi <guilhermerissi04112006@gmail.com>
2025-09-21 20:47:29 -0300

    refactor: remove unused auth interceptor from app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,6 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from '../interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,7 +13,6 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(withEventReplay()),
     provideHttpClient(
       withFetch(),
-      withInterceptors([authInterceptor])
     ),
   ]
 };
