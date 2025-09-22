028ea4c1408ba15d91637b7802096187988be121
glrmrissi <guilhermerissi04112006@gmail.com>
2025-09-22 11:24:48 -0300

    feat: implement header and nav components with HTML, SCSS, and TypeScript integration

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { KanbanContainerComponent } from "../home/kanban-container/kanban-container.component";
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-home',
-  imports: [KanbanContainerComponent, KanbanContainerComponent],
+  imports: [KanbanContainerComponent, KanbanContainerComponent, HeaderComponent],
   templateUrl: './home.html',
   styleUrls: ['./home.scss']
 })
